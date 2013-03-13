47379cca02a23fe254dc749b42723efa36e32450
Fawad Rafi <fawadrafi@yahoo.com>
2013-03-13 01:25:13 -0400

    Initial drop

---
+++ WCS/WCSEngine/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WCSEngine::Application.config.secret_token = 'a7183c509d850504241a08d61e10aca514e9f25e9f6a84ba2b5a2c5f64ac3e48c516c12a135c3cbf905efa226bf02f03526accfd7f454cc5d523215b6d9438e6'
