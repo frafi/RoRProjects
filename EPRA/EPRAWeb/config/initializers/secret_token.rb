310e234e7cfb65603ced7d7f1d3091904f1fb5b1
Fawad Rafi <fawadrafi@yahoo.com>
2013-03-13 01:15:25 -0400

    Initial Drop

---
+++ EPRA/EPRAWeb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EPRAWeb::Application.config.secret_token = 'f1d8c6611fa0f9186a54ec0e84ad7289a295b3226be460ba3841ce5405983e6271bce8d7fcbbf534496182c9507f6118a6f56e30176c094d1b9432d2cb858b68'
